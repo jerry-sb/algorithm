d6c0a7b0dd5c5c0f55daed9bb79db4464c31cfbb
mysimba <mugbo1410@gmail.com>
2024-10-29 12:40:09 +0900

    week1 문제 업로드

---
+++ week1/인사고과.js
@@ -0,0 +1,23 @@
+function solution(scores) {
+    const wanho = scores[0];
+    const wanhoSum = wanho[0] + wanho[1];
+
+    scores.sort((a,b) => a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]);
+
+    let answer = 1;
+    let maxScore = 0;
+
+    for (let score of scores) {
+        if (score[1] < maxScore) {
+            if (score === wanho) return -1;
+            continue;
+        } else {
+            maxScore = Math.max(maxScore, score[1]);
+            if (score[0] < score[1] > wanhoSum)  {
+                answer++;
+            }
+        }
+    }
+
+    return answer;
+}
